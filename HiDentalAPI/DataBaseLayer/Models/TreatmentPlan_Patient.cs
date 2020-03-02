3ca7eadb9fd5d2b952276996f27858cbc9086293
Lony Mejia <lonymejia@Lonys-MacBook-Pro.local>
2020-03-01 23:14:04 -0400

    Model pending(TreatmentPlan) Ready

---
+++ HiDentalAPI/DataBaseLayer/Models/TreatmentPlan_Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BussinesLayer.Contracts;
+
+namespace DatabaseLayer.Models
+{
+    public class TreatmentPlan_Patientcs : CommonsProperty
+    {
+
+        public Guid PlanTratamientoID { get; set; }
+        public Treatment_plan Treatment_plan { get; set; }
+
+        public Guid ConsultationID { get; set; }
+        public Consultation Consultation { get; set; }
+
+        public Guid PatientID { get; set; }
+        public Patient Patient { get; set; }
+
+        public Guid UserID { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
